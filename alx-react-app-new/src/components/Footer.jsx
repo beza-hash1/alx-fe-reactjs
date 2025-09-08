2735141c96a89679d2a13787265dd69285ee8d16
beza-hash1 <bezawitabera512@gmail.com>
2025-09-08 23:24:15 +0300

    Updated Footer and MainContent.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,9 +1,9 @@
-function Footer() {
+const Footer = () => {
   return (
-    <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
       <p>© 2023 City Lovers</p>
     </footer>
   );
-}
+};
 
 export default Footer;
