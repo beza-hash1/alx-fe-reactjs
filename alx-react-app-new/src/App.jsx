399a42670bed2ab4b7e0d85f30b304900c3df758
beza-hash1 <bezawitabera512@gmail.com>
2025-09-08 23:36:11 +0300

    Add all components properly

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,22 +1,23 @@
+import './App.css';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile';
-
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Counter from './components/Counter';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
       <MainContent />
-      <UserProfile 
-  name="Alice" 
-  age={25} 
-  bio="Loves hiking and photography" 
-/>
-
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+      <Counter />
       <Footer />
-    </>
+    </div>
   );
 }
 
